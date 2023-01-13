c5ffeaf209c7878bc70aa5eb48fc386d5f6f45c4
Muhammad Ayaz Hasan <ayazhasan97@gmail.com>
2023-01-13 08:32:27 +0500

    Add files via upload

---
+++ Semester5/Software Construction&Development/PRACTICE EXERCISE(JAVA,JSP)/addservlet.java
@@ -0,0 +1,10 @@
+package Exercise1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+public class addservlet extends HttpServlet {
+
+}
